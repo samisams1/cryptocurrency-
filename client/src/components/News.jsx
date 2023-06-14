724f7e8ea0095fdbf9e9be7c65d6d8ae5006387d
samisams1 <forsamisams@gmail.com>
2023-06-14 09:55:05 +0300

    update news page

--- client/src/components/News.jsx
+++ client/src/components/News.jsx
@@ -1,25 +1,37 @@
 import { Card, Col, Row, Select } from 'antd';
+import { Option } from 'antd/es/mentions';
 import Title from 'antd/es/typography/Title';
 import React from 'react'
+import { useState } from 'react';
+import { useGetCryptosQuery } from '../services/cryptoApi';
 import { useGetCryptoNewsQuery } from '../services/cryptoNewsApi'
 
 const   News =({simplified})=> {
-const {data:cryptoNews}=useGetCryptoNewsQuery({newsCategory:'Cryptocurrency',count:simplified?10:100});
+//const {data:cryptoNews}=useGetCryptoNewsQuery({newsCategory:'Cryptocurrency',count:simplified?10:100});
+const {data}=useGetCryptosQuery(100);
+const [newsCategory, setNewsCategory] = useState('Cryptocurrency');
+const { data: cryptoNews } = useGetCryptoNewsQuery({ newsCategory, count: simplified ? 6 : 12 });
 const demoImage = 'https://www.bing.com/th?id=OVFT.mpzuVZnv8dwIMRfQGPbOPC&pid=News';
 console.log(cryptoNews)
+
 if(!cryptoNews?.value) return 'Loading ...';
 
 return (
   <Row gutter={[24,24]}>
 
-      <Col span ={24}>
-        <Select
-          showSearch
-          className='select-news'
-          placeholder="Select a Crypto"
-          optionFilterProp='children'
-          > </Select>
-      </Col>
+<Col span={24}>
+          <Select
+            showSearch
+            className="select-news"
+            placeholder="Select a Crypto"
+            optionFilterProp="children"
+            onChange={(value) => setNewsCategory(value)}
+            filterOption={(input, option) => option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
+          >
+            <Option value="Cryptocurency">Cryptocurrency</Option>
+            {data?.data?.coins?.map((currency) => <Option value={currency.name}>{currency.name}</Option>)}
+          </Select>
+        </Col>
     
 
     
